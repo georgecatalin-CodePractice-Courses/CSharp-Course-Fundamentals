428c148c21e9e1360d61836c3175511e2a947611
Calin <George Calin@lacon.de>
2019-10-02 16:20:32 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace src
+{
+
+    // Issue of The type or namespace name 'System' could not be found>> Solution: vanishes if I target netcoreapp2.2.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x=34.1;
+            double y=10.3;
+
+            double result=x+y;
+
+            System.Console.WriteLine(result);
+
+            if (args.Length>0)
+            {
+                 Console.WriteLine($"Hello {args[0]}!"); 
+            }
+            else{
+                Console.WriteLine("Help needed");
+            }
+          
+        }
+    }
+}
