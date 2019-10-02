1e6ad8e1cb7e661b08a6c73012b3f8a749834304
Calin <George Calin@lacon.de>
2019-10-02 17:15:45 +0300

    Initial version of the Program.cs class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace src
 {
@@ -8,8 +9,28 @@ namespace src
     {
         static void Main(string[] args)
         {
-           
-          
+           List<double> grades=new List<double>(){12.7,10.3,6.11,4.1};
+           grades.Add(56.1);
+
+           double result=0.0d;
+
+           foreach (double grade in grades)
+           {
+               result+=grade;
+           }
+    
+            result/=grades.Count;
+
+            System.Console.WriteLine($"The average grade is {result:N1}");
+
+            if (args.Length>0)
+            {
+                System.Console.WriteLine($"Hello, {arg[0]}");
+            }
+            else
+            {
+                System.Console.WriteLine("Help wanted");
+            }
         }
     }
 }
